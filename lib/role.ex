e077292589c958607e09aecb11117c2fd99f814c
Ivan Kozik <ivan@ludios.org>
2017-02-20 01:50:23 +0000

    Also install CUDA and OpenCL support

--- lib/role.ex
+++ lib/role.ex
@@ -5,8 +5,9 @@ defmodule RoleNvidia do
 	Util.declare_external_resources("files")
 
 	def role(_tags \\ []) do
+		version = 378
 		%{
-			desired_packages: ["nvidia-378", "nvidia-settings"],
+			desired_packages: ["nvidia-#{version}", "libcuda1-#{version}", "nvidia-opencl-icd-#{version}", "nvidia-settings"],
 			apt_keys:         [Util.content("files/apt_keys/1118213C Launchpad PPA for Graphics Drivers Team.txt")],
 			apt_sources:      ["deb http://ppa.launchpad.net/graphics-drivers/ppa/ubuntu xenial main"],
 		}
