b9096aadcb46337774dbe68a015efb1954f96554
Ivan Kozik <ivan@ludios.org>
2018-07-17 11:51:07 +0000

    Update for nvidia 390.77

--- lib/role.ex
+++ lib/role.ex
@@ -5,7 +5,7 @@ defmodule RoleNvidia do
 	import Util, only: [conf_file: 1, conf_dir: 1]
 	Util.declare_external_resources("files")
 
-	def role(tags \\ []) do
+	def role(_tags \\ []) do
 		%{
 			desired_packages: [
 				"xserver-xorg-legacy",
@@ -20,13 +20,6 @@ defmodule RoleNvidia do
 				"nvidia-vdpau-driver",
 				"nvidia-settings",
 				"nvidia-smi",
-
-				# GLVND flavor, as per debian/nvidia-driver.README.Debian.in
-				"libgl1-nvidia-glvnd-glx",
-				"libgl1-glvnd-nvidia-glx",
-				"nvidia-egl-icd",
-				"libglx0-glvnd-nvidia",
-				"libegl1-glvnd-nvidia",
 				"nvidia-vulkan-common",
 
 				# For --hwdec=cuda in mpv
@@ -34,7 +27,7 @@ defmodule RoleNvidia do
 
 				# For Steam, which needs all of these on NVIDIA
 				"libdrm2:i386",
-				"libgl1-glvnd-nvidia-glx:i386",
+				"libgl1:i386",
 				"libglx-nvidia0:i386",
 			],
 			post_install_unit: %All{units: [
